0e038fe17cc53eb3f6c5b41b555158d7f4ea3be8
WISEMAN <ahiwewisdom111@gmail.com>
2026-07-14 17:33:34 +0100

    updated

--- test/DecentralBank.tests.js
+++ test/DecentralBank.tests.js
@@ -6,7 +6,7 @@ require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract('decentralBank', (accounts) => {
+contract('decentralBank', ([owner, customer]) => {
      //this is a session for testing 
 
      // for the sake of reusablity we have to use the 
@@ -23,11 +23,12 @@ contract('decentralBank', (accounts) => {
           tether = await Tether.new();
           rwd = await RWD.new();
           decentralBank = await DecentralBank.new(rwd.address, tether.address);
-          transfer = await rwd.transfer(decentralBank.address, tokens('1000000'));
+          // I used await because I didn't delare the transfer function
+          await rwd.transfer(decentralBank.address, tokens('1000000'));
           //for the sake of testing we are going to transfer 100 tether tokens to the investor
           //this looks like exactly what we did in the deployement 
           //script but we are now adding where the token is coming from using our "from" object 
-           await tether.transfer(accounts[1], tokens('100'), {from: accounts[0]} )
+           await tether.transfer(customer, tokens('100'), {from: owner} )
      })
 
      describe('Mock Tether Deployement', () => {
@@ -38,13 +39,27 @@ contract('decentralBank', (accounts) => {
           });
      } );
 
-     describe('Reward Token', () => {
+     describe('Reward Token Deployment ', () => {
           it('matches name successfully ', async() =>{
            
                const name = await rwd.name();
                assert.equal(name, 'Reward Token');
           } )
      })
+
+     
+     describe('DecentralBank Deployment', () => {
+          it('matches name successfully', async () => {
+               const name = await decentralBank.name();
+               assert.equal(name, 'Decentral Bank');
+          });
+
+          it('contract has tokens', async () => {
+               let balance = await rwd.balanceOf(decentralBank.address)
+               assert.equal(balance, tokens('1000000'))
+          })
+     });
+
 
 })
 
